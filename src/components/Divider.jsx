8cda2e7df052a499d0e4dfb251ae9b6577a85ebd
Esra <esraayaareb@gmail.com>
2024-12-08 18:14:56 +0300

    static design done

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,10 +1,10 @@
 export default function Divider({ number }) {
   return (
-    <>
+    <div className="divider">
       <div class="vertical_line"></div>
       <div className="number">
         <span className="text-5">{number}</span>
       </div>
-    </>
+    </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <h2 className="text-2">Experience more together</h2>
+        <p className="text-4">
+          Stay connected with reliable HD meetings and unlimited one-on-one and
+          group video sessions.
+        </p>
+        <a href="" className="btn btn_secondary text-5">
+          Download <span>v1.3</span>
+        </a>
+      </div>
+    </footer>
+  );
 }
